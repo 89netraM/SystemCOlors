0636e10d499c148df04dea3ed7d039ed1a297ff5
Mårten Åsberg <marten.asberg@outlook.com>
2019-04-23 12:10:22 +0200

    Added Windows Registry reader

---
+++ src/windows/WindowsRegistry.java
@@ -0,0 +1,57 @@
+package windows;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+// Borrowed from: https://transoceanic.blogspot.com/2011/12/java-read-key-from-windows-registry.html
+
+class WindowsRegistry {
+	static String ReadKey(String path, String key) {
+		try {
+			Process p = Runtime.getRuntime().exec("reg query \"" + path + "\" /v " + key);
+
+			StreamReader sr = new StreamReader(p.getInputStream());
+			sr.start();
+			p.waitFor();
+			sr.join();
+
+			String[] result = sr.toString().split("\\s+");
+			if (result.length > 1) {
+				return result[result.length - 1];
+			}
+		}
+		catch (Exception ignored) { }
+
+		return null;
+	}
+
+	private WindowsRegistry() { }
+
+	private static class StreamReader extends Thread {
+		private final InputStream is;
+		private final StringWriter sw;
+
+		StreamReader(InputStream is) {
+			this.is = is;
+			this.sw = new StringWriter();
+		}
+
+		public void run() {
+			try {
+				int s;
+				while ((s = is.read()) != -1) {
+					sw.write(s);
+				}
+			}
+			catch (IOException ignored) { }
+
+			sw.flush();
+		}
+
+		@Override
+		public String toString() {
+			return sw.toString();
+		}
+	}
+}
